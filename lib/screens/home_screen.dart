24a402a001756dc7ecab9fdd6ee8c0679bb8f645
AldoRH <aldo.romeroh@gmail.com>
2024-02-15 21:31:20 -0600

    Create home_screen
    Pantalla principal de la aplicación que mostrara los datos guardados

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:preferences_app/share_preferences/preferences.dart';
+import 'package:preferences_app/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  static const String routeName = 'Home';
+  
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      drawer: const SideMenu(),
+      body:  Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('isDarkmode: ${Preferences.isDarkMode} '),
+          const Divider(),
+          Text('Género: ${Preferences.gender}'),
+          const Divider(),
+          Text('Nombre de usuario: ${Preferences.name}')
+        ],
+      ),
+
+    );
+  }
+}
